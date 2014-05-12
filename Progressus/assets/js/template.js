5aa22c83c405bb54c48c2f0983d126e7ffac003a
Sergio Fruto <fruto.sergio@gmail.com>
2014-05-11 21:08:47 -0300

    Added a bunch of stuff

--- Progressus/assets/js/template.js
+++ Progressus/assets/js/template.js
@@ -11,3 +11,6 @@ jQuery(document).ready(function($) {
 	});
 
 });
+
+// challonge js
+$('.demo_iframe').challonge('tecnoprueba', {subdomain: '', theme: '1', multiplier: '1.0', match_width_multiplier: '1.0', show_final_results: '0', show_standings: '0'});
